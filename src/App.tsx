027f80fe250e8bc65f198e00d32809f6a388c0c2
Danny Harding <dannyharding10@gmail.com>
2021-02-09 21:29:12 -0600

    Move Ground to separate file Add grass texture

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
-import React, { useState, useRef, useEffect, useMemo } from 'react'
+import React, { useState, useRef, useEffect, useMemo, Suspense } from 'react'
 import * as THREE from 'three'
-import { Canvas, useThree, extend, useFrame } from 'react-three-fiber'
+import { Canvas, useThree, extend } from 'react-three-fiber'
 import { FlyControls } from 'three/examples/jsm/controls/FlyControls.js'
 import Tower from './assets/Tower'
 import Enemy from './assets/Enemy'
 import Bullet from './assets/Bullet'
-import {
-	GRID_CELL_SIZE,
-	GRID_SIZE,
-	MAP_WIDTH,
-	GROUND_HEIGHT,
-	ENEMY_SIZE,
-	BULLET_SPEED,
-	ENEMY_SPEED,
-} from './constants'
+import Ground from './components/Ground'
+import { GRID_CELL_SIZE, GRID_SIZE, MAP_WIDTH, ENEMY_SIZE, ENEMY_SPEED } from './constants'
 import './App.css'
 import {
 	getTimeToTarget,
@@ -22,6 +15,7 @@ import {
 	calculateTimeToInterception,
 	getSpeedVector,
 } from './helpers'
+
 extend({ FlyControls })
 
 function Camera() {
@@ -114,141 +108,105 @@ function App() {
 	return (
 		<div className="App">
 			<Canvas>
-				<ambientLight intensity={0.3} />
-				<pointLight position={[MAP_WIDTH / 2, 400, MAP_WIDTH / 2]} />
-				<axesHelper args={[1000]} />
-				<gridHelper
-					args={[MAP_WIDTH * 2, (MAP_WIDTH / GRID_CELL_SIZE) * 2, 'white', 'gray']}
-					rotation={[Math.PI / 2, 0, 0]}
-				/>
-				<Camera />
-				{/* <Controls /> */}
-				{towers.map((tower) => (
-					<Tower
-						x={tower.x * GRID_CELL_SIZE + GRID_CELL_SIZE / 2}
-						y={tower.y * GRID_CELL_SIZE + GRID_CELL_SIZE / 2}
-						color={'hotpink'}
-						target={firstEnemy}
-						onShoot={(origin, bulletSpeed) => {
-							const timeToInterception = calculateTimeToInterception(
-								[firstEnemy.x, firstEnemy.y],
-								getSpeedVector(firstEnemy, pathArray),
-								[origin[0], origin[1]],
-								bulletSpeed
-							)
-							if (!timeToInterception) {
-								throw new Error('Tower is unable to shoot that thing')
-							}
-
-							const interceptionPoint = getFutureLocation(firstEnemy, pathArray, timeToInterception)
-							console.log(firstEnemy.x, firstEnemy.y, interceptionPoint)
-
-							setEnemies((enemies) => {
-								const targettedEnemy = enemies[firstEnemy.id]
-								return {
-									...enemies,
-									[targettedEnemy.id]: {
-										...targettedEnemy,
-										futureHp: targettedEnemy.futureHp - 10,
-									},
-								}
-							})
-
-							setBullets((bullets) => [
-								...bullets,
-								{
-									origin,
-									destination: [...interceptionPoint, ENEMY_SIZE / 2],
-									speed: bulletSpeed,
-									startTime: new Date(),
-									targetEnemy: firstEnemy.id,
-								},
-							])
-						}}
-						key={tower.id}
+				<Suspense fallback={null}>
+					<ambientLight intensity={0.3} />
+					<pointLight position={[MAP_WIDTH / 2, 400, MAP_WIDTH / 2]} />
+					<axesHelper args={[1000]} />
+					<gridHelper
+						args={[MAP_WIDTH * 2, (MAP_WIDTH / GRID_CELL_SIZE) * 2, 'white', 'gray']}
+						rotation={[Math.PI / 2, 0, 0]}
 					/>
-				))}
-				<Ground
-					onClick={(x, y) => setTowers((towers) => [...towers, { id: nextTowerId++, x, y }])}
-					path={path}
-				/>
-				<Enemies
-					path={pathArray}
-					enemies={enemies}
-					onUpdate={(enemyId, update) => {
-						setEnemies((enemies) => {
-							const newEnemies = { ...enemies }
-							newEnemies[enemyId] = { ...newEnemies[enemyId], ...update }
-							return newEnemies
-						})
-					}}
-				/>
-				<Bullets
-					bullets={bullets}
-					removeBullet={(bulletToRemove) => {
-						setEnemies((enemies) => {
-							const newEnemies = { ...enemies }
-							const hitEnemyId = bulletToRemove.targetEnemy
-							newEnemies[hitEnemyId] = {
-								...newEnemies[hitEnemyId],
-								hp: newEnemies[hitEnemyId].hp - 10,
-							}
-							if (newEnemies[hitEnemyId].hp === 0) {
-								delete newEnemies[hitEnemyId]
-							}
-							return newEnemies
-						})
-						setBullets((bullets) => bullets.filter((bullet) => bullet !== bulletToRemove))
-					}}
-				/>
+					<Camera />
+					{/* <Controls /> */}
+					{towers.map((tower) => (
+						<Tower
+							x={tower.x * GRID_CELL_SIZE + GRID_CELL_SIZE / 2}
+							y={tower.y * GRID_CELL_SIZE + GRID_CELL_SIZE / 2}
+							color={'hotpink'}
+							target={firstEnemy}
+							onShoot={(origin, bulletSpeed) => {
+								const timeToInterception = calculateTimeToInterception(
+									[firstEnemy.x, firstEnemy.y],
+									getSpeedVector(firstEnemy, pathArray),
+									[origin[0], origin[1]],
+									bulletSpeed
+								)
+								if (!timeToInterception) {
+									throw new Error('Tower is unable to shoot that thing')
+								}
+
+								const interceptionPoint = getFutureLocation(
+									firstEnemy,
+									pathArray,
+									timeToInterception
+								)
+								console.log(firstEnemy.x, firstEnemy.y, interceptionPoint)
+
+								setEnemies((enemies) => {
+									const targettedEnemy = enemies[firstEnemy.id]
+									return {
+										...enemies,
+										[targettedEnemy.id]: {
+											...targettedEnemy,
+											futureHp: targettedEnemy.futureHp - 10,
+										},
+									}
+								})
+
+								setBullets((bullets) => [
+									...bullets,
+									{
+										origin,
+										destination: [...interceptionPoint, ENEMY_SIZE / 2],
+										speed: bulletSpeed,
+										startTime: new Date(),
+										targetEnemy: firstEnemy.id,
+									},
+								])
+							}}
+							key={tower.id}
+						/>
+					))}
+					<Ground
+						onClick={(x, y) => setTowers((towers) => [...towers, { id: nextTowerId++, x, y }])}
+						path={path}
+					/>
+					<Enemies
+						path={pathArray}
+						enemies={enemies}
+						onUpdate={(enemyId, update) => {
+							setEnemies((enemies) => {
+								const newEnemies = { ...enemies }
+								newEnemies[enemyId] = { ...newEnemies[enemyId], ...update }
+								return newEnemies
+							})
+						}}
+					/>
+					<Bullets
+						bullets={bullets}
+						removeBullet={(bulletToRemove) => {
+							setEnemies((enemies) => {
+								const newEnemies = { ...enemies }
+								const hitEnemyId = bulletToRemove.targetEnemy
+								newEnemies[hitEnemyId] = {
+									...newEnemies[hitEnemyId],
+									hp: newEnemies[hitEnemyId].hp - 10,
+								}
+								if (newEnemies[hitEnemyId].hp === 0) {
+									delete newEnemies[hitEnemyId]
+								}
+								return newEnemies
+							})
+							setBullets((bullets) => bullets.filter((bullet) => bullet !== bulletToRemove))
+						}}
+					/>
+				</Suspense>
 			</Canvas>
 		</div>
 	)
 }
 
 let nextTowerId = 0
-
-function Ground({ onClick, path }: { onClick: (number, number) => mixed }) {
-	const grounds = []
-	const [hoveredGrid, setHoveredGrid] = useState(null)
-
-	for (let i = 0; i < GRID_SIZE; i++) {
-		for (let j = 0; j < GRID_SIZE; j++) {
-			const isInPath = path[i]?.[j]
-			let color = hoveredGrid && hoveredGrid[0] === i && hoveredGrid[1] === j ? 'gold' : 'green'
-			if (isInPath) {
-				color = 'brown'
-			}
-			grounds.push(
-				<mesh
-					position={[
-						i * GRID_CELL_SIZE + GRID_CELL_SIZE / 2,
-						j * GRID_CELL_SIZE + GRID_CELL_SIZE / 2,
-						-GROUND_HEIGHT / 2,
-					]}
-					onClick={(e) => {
-						if (isInPath) {
-							return
-						}
-						e.stopPropagation()
-						onClick(i, j)
-					}}
-					onPointerOver={(e) => {
-						e.stopPropagation()
-						setHoveredGrid([i, j])
-					}}
-					onPointerOut={() => setHoveredGrid(null)}
-					key={`i${i}j${j}`}
-				>
-					<boxBufferGeometry args={[GRID_CELL_SIZE, GRID_CELL_SIZE, GROUND_HEIGHT]} />
-					<meshStandardMaterial color={color} />
-				</mesh>
-			)
-		}
-	}
-
-	return grounds
-}
 
 function Enemies({
 	path,
---
+++ src/components/Ground.tsx
@@ -0,0 +1,98 @@
+import React, { useState } from 'react'
+import { useLoader } from 'react-three-fiber'
+import { TextureLoader } from 'three/src/loaders/TextureLoader.js'
+import { GRID_SIZE, GRID_CELL_SIZE, GROUND_HEIGHT } from '../constants'
+import grassImage from '../assets/grasslight-big.jpg'
+
+export default function Ground({ onClick, path }: { onClick: (x: number, y: number) => void }) {
+	const grounds = []
+	const [hoveredGrid, setHoveredGrid] = useState<[number, number] | null>(null)
+
+	for (let i = 0; i < GRID_SIZE; i++) {
+		for (let j = 0; j < GRID_SIZE; j++) {
+			const isInPath = path[i]?.[j]
+			const position: [number, number, number] = [
+				i * GRID_CELL_SIZE + GRID_CELL_SIZE / 2,
+				j * GRID_CELL_SIZE + GRID_CELL_SIZE / 2,
+				-GROUND_HEIGHT / 2,
+			]
+			if (hoveredGrid && hoveredGrid[0] === i && hoveredGrid[1] === j) {
+				grounds.push(
+					<HoveredGrass
+						position={position}
+						onPointerOver={(e) => e.stopPropagation()}
+						onPointerOut={() => setHoveredGrid(null)}
+						onClick={(e) => {
+							e.stopPropagation()
+							onClick(i, j)
+						}}
+					/>
+				)
+			} else if (isInPath) {
+				grounds.push(
+					<Path
+						position={[
+							i * GRID_CELL_SIZE + GRID_CELL_SIZE / 2,
+							j * GRID_CELL_SIZE + GRID_CELL_SIZE / 2,
+							-GROUND_HEIGHT / 2,
+						]}
+					/>
+				)
+			} else {
+				grounds.push(
+					<Grass
+						position={position}
+						onPointerOver={(e) => {
+							e.stopPropagation()
+							console.log(e)
+							console.log('pointer over grass ', i, j)
+							if (e.faceIndex === 8 || e.faceIndex === 9) {
+								// Only accept events on the top of the cube
+								setHoveredGrid([i, j])
+							}
+						}}
+					/>
+				)
+			}
+		}
+	}
+
+	return grounds
+}
+
+function Path({ position }: { position: [number, number, number] }) {
+	return (
+		<mesh
+			position={position}
+			onClick={(e) => {
+				e.stopPropagation()
+			}}
+			onPointerOver={(e) => {
+				e.stopPropagation()
+			}}
+		>
+			<boxBufferGeometry args={[GRID_CELL_SIZE, GRID_CELL_SIZE, GROUND_HEIGHT]} />
+			<meshStandardMaterial color={'brown'} />
+		</mesh>
+	)
+}
+
+function Grass({ position, ...rest }: { position: [number, number, number] }) {
+	const grassTexture = useLoader(TextureLoader, grassImage)
+
+	return (
+		<mesh position={position} {...rest}>
+			<boxBufferGeometry args={[GRID_CELL_SIZE, GRID_CELL_SIZE, GROUND_HEIGHT]} />
+			<meshStandardMaterial map={grassTexture} />
+		</mesh>
+	)
+}
+
+function HoveredGrass({ position, ...rest }: { position: [number, number, number] }) {
+	return (
+		<mesh position={position} {...rest}>
+			<boxBufferGeometry args={[GRID_CELL_SIZE, GRID_CELL_SIZE, GROUND_HEIGHT]} />
+			<meshStandardMaterial color={'gold'} />
+		</mesh>
+	)
+}
